0cabbbccac9b5ec6faf0ecfc8a62f8832db7d08a
Jove <jovezhong@gmail.com>
2013-02-14 13:20:32 +0800

    remove dead code

--- content.js
+++ content.js
@@ -19,44 +19,5 @@ function handleContextMenu(event){
 }
 
 function handleMessage(event){
-	//console.log("entry-- content:handleMessage");
-    // Always check the name of the message that you want to handle.
-    if (event.name !== "searchWord")
-        return;
-    
-    if (!lastRightClickedElement) 
-        return;
-    
-    // Make sure the timestamp of when lastRightClickedElement is saved matches the one sent in the message.
-    // This message is sent to every frame on this page, and we don't want to hide lastRightClickedElement in a
-    // different frame that was saved from a previous contextmenu event.
-    if (lastContextMenuEventTime === event.message.timestamp){
-    	//the real work of this extension
-    	alert("about to show json: "+event.message.responseText);
-
-    	/*
-    	alert(lastRightClickedText)
-    	var xmlhttp = new XMLHttpRequest();
-
-		xmlhttp.open('POST', 'http://www.shanbay.com/api/word/jove', true);
-		xmlhttp.onreadystatechange = handleServerResponse;
-		xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-		xmlhttp.send();
-
-		function handleServerResponse() {    
-		    if (xmlhttp.readyState == 4) {
-		        alert(xmlhttp.getAllResponseHeaders());
-		        if (xmlhttp.status == 200) {
-		            alert("send");
-		        } else {
-		            alert("error");
-		        }
-		    } 
-		}
-		*/
-    }
-    
-    lastRightClickedElement = null;
-    lastContextMenuEventTime = null;
-    lastRightClickedText = null;
+	//not implemented yet
 }
